87b880600357412118a0d12d88a0b4f881efe56d
suicida0dj <suicidadj1@gmail.com>
2022-10-05 21:15:03 +0200

    Haciendo el puto ejercicio 5

--- src/Ejercicios/Ejercicio5.java
+++ src/Ejercicios/Ejercicio5.java
@@ -19,36 +19,35 @@ public class Ejercicio5 {
                         for (int l = 0; l < 10; l++) {
                             if (i == 0 && j == 0 && k == 0 && h == 0 && l == 0) {
                                 System.out.println("XXXXX");
-                            }else if(i == 3 && j == 3 && k == 3 && h == 3 && l == 3){
+                            } else if (i == 3 && j == 3 && k == 3 && h == 3 && l == 3) {
                                 System.out.println("E-E-E-E-E");
-                            }else if(i == 3 && j == 3 && k == 3 && h == 3){
-                                System.out.println("E-E-E-E-"+l);
-                            }else if(i == 3 && j == 3 && k == 3){
-                                System.out.println("E-E-E-"+h+"-"+l);
-                            }else if(i == 3 && j == 3){
-                                System.out.println("E-E-"+k+"-"+h+"-"+l);
-                            }else if(i == 3){
-                                System.out.println("E-"+j+"-"+k+"-"+h+"-"+l);
-                            }else if( h == 3 && l == 3){
+                            } else if (i == 3 && j == 3 && k == 3 && h == 3) {
+                                System.out.println("E-E-E-E-" + l);
+                            } else if (i == 3 && j == 3 && k == 3) {
+                                System.out.println("E-E-E-" + h + "-" + l);
+                            } else if (i == 3 && j == 3) {
+                                System.out.println("E-E-" + k + "-" + h + "-" + l);
+                            } else if (h == 3 && l == 3) {
                                 System.out.println(i + "-" + j + "-" + k + "-E-E");
-                            }else if(i == 3 && j == 3 && k == 3 && h == 3){
-                                System.out.println("E-E-E-E-"+l);
-                            }else if(i == 3 && j == 3 && k == 3){
-                                System.out.println("E-E-E-"+h+"-"+l);
-                            }else if(i == 3 && j == 3){
-                                System.out.println("E-E-"+k+"-"+h+"-"+l);
-                            }else if(i == 3){
-                                System.out.println("E-"+j+"-"+k+"-"+h+"-"+l);
-                            }else if(l == 3){
+                            } else if (k == 3 && l == 3) {
+                                System.out.println(i + "-" + j + "-E-" + h + "-E");
+                            } else if (j == 3 && l == 3) {
+                                System.out.println(i + "-E-" + k + "-" + h + "-E");
+                            } else if (i == 3 && l == 3) {
+                                System.out.println("E-" + j + "-" + k + "-" + h + "-E");
+                            } else if (i == 3) {
+                                System.out.println("E-" + j + "-" + k + "-" + h + "-" + l);
+                            } else if (l == 3) {
                                 System.out.println(i + "-" + j + "-" + k + "-" + h + "-E");
-                            }else if(h == 3){
+                            } else if (h == 3) {
                                 System.out.println(i + "-" + j + "-" + k + "-E-" + l);
-                            }else if(k == 3){
+                            } else if (k == 3) {
                                 System.out.println(i + "-" + j + "-E-" + h + "-" + l);
-                            }else if(j == 3){
+                            } else if (j == 3) {
                                 System.out.println(i + "-E-" + k + "-" + h + "-" + l);
-
-                            }else {
+                            } else if (i == 3) {
+                                System.out.println("E-" + j + "-" + k + "-" + h + "-" + l);
+                            } else {
                                 System.out.println(i + "-" + j + "-" + k + "-" + h + "-" + l);
                             }
                         }
